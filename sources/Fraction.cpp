85259e64a476745f6d5980566758871746520379
Aleksandr Tarasenko <work.tarasenko@gmail.com>
2014-07-04 11:30:09 +0300

    putted all code into namespace FractionLibrary, added definition for operator+(Fraction, Fraction)

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -2,49 +2,73 @@
 
 using namespace FractionLibrary;
 
-Fraction::Fraction(const fraction_type numerator, 
-                   const fraction_type denominator)
-    throw (DivisionByZero)
+namespace FractionLibrary
 {
-    if(denominator == 0)
+    Fraction::Fraction(const fraction_type numerator, 
+                       const fraction_type denominator)
+        throw (DivisionByZero)
     {
-        throw DivisionByZero();
+        if(denominator == 0)
+        {
+            throw DivisionByZero();
+        }
+        setNumerator(numerator);
+        setDenominator(denominator);
     }
-    setNumerator(numerator);
-    setDenominator(denominator);
-}
 
-fraction_type Fraction::getNumerator() const
-{
-    return numerator_;
-}
-
-fraction_type Fraction::getDenominator() const
-{
-    return denominator_;
-}
-
-fraction_type Fraction::getDefaultNumerator()
-{
-    return 0;
-}
-
-fraction_type Fraction::getDefaultDenominator()
-{
-    return 1;
-}
-
-void Fraction::setNumerator(const fraction_type numerator)
-{
-    numerator_ = numerator;
-}
-
-void Fraction::setDenominator(const fraction_type denominator)
-    throw(DivisionByZero)
-{
-    if(denominator == 0)
+    fraction_type Fraction::getNumerator() const
     {
-        throw DivisionByZero();
+        return numerator_;
     }
-    denominator_ = denominator;
+
+    fraction_type Fraction::getDenominator() const
+    {
+        return denominator_;
+    }
+
+    fraction_type Fraction::getDefaultNumerator()
+    {
+        return 0;
+    }
+
+    fraction_type Fraction::getDefaultDenominator()
+    {
+        return 1;
+    }
+
+    void Fraction::setNumerator(const fraction_type numerator)
+    {
+        numerator_ = numerator;
+    }
+
+    void Fraction::setDenominator(const fraction_type denominator)
+        throw(DivisionByZero)
+    {
+        if(denominator == 0)
+        {
+            throw DivisionByZero();
+        }
+        denominator_ = denominator;
+    }
+    
+    Fraction operator+(const Fraction& left_operand, const Fraction& right_operand)
+    {
+        Fraction result;
+        if(left_operand.getDenominator() == right_operand.getDenominator())
+        {
+            result.setDenominator(left_operand.getDenominator());
+            result.setNumerator(left_operand.getNumerator() + 
+                                right_operand.getNumerator());
+        }
+        else
+        {
+            result.setDenominator(left_operand.getDenominator() * 
+                                  right_operand.getDenominator());
+            result.setNumerator(left_operand.getNumerator() * 
+                                right_operand.getDenominator() + 
+                                right_operand.getNumerator() * 
+                                left_operand.getDenominator());
+        }
+        return result;
+    }
 }
