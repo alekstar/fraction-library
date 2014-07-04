2d5aef862429aeb16cb97d1cae7c084317416c3b
Aleksandr Tarasenko <work.tarasenko@gmail.com>
2014-07-04 11:31:17 +0300

    uncommented TEST(Addition, ResultShouldBe2by2) and run it

--- tests/FractionTest.cpp
+++ tests/FractionTest.cpp
@@ -83,11 +83,11 @@ TEST(SetDenominator, ShouldSetDenominatorToMinus100)
     EXPECT_EQ(-100, fraction.getDenominator());
 }
 
-//TEST(Addition, ResultShouldBe2by2)
-//{
-//    Fraction operand1(1, 2);
-//    Fraction operand2(1, 2);
-//    Fraction result = operand1 + operand2;
-//    EXPECT_EQ(2, result.getNumerator());
-//    EXPECT_EQ(2, result.getDenominator());
-//}
+TEST(Addition, ResultShouldBe2by2)
+{
+    Fraction operand1(1, 2);
+    Fraction operand2(1, 2);
+    Fraction result = operand1 + operand2;
+    EXPECT_EQ(2, result.getNumerator());
+    EXPECT_EQ(2, result.getDenominator());
+}
